cd7e17e55b8d8f30f400ff5716ccdd16d885d729
rubicus <laba625@yandex.ru>
2024-11-01 15:36:27 +0300

    ДЗ4 full. Address shift + bitreverse shift + rewrite programm syntax

--- ДЗ4/main.py
+++ ДЗ4/main.py
@@ -42,16 +42,23 @@ class Assembler:
 
     def bin_read(self, a: int, b: int, c: int) -> bytes:
         self.log({"A": a, "B": b, "C": c})
+
         a, b, c = self.paginate(a, 3), self.paginate(b, 4), self.paginate(c, 4)
         text = a + b + c + "0" * 29
         return int(text, 2).to_bytes(5, "big")
 
-    def bin_bitreverse(self, a: int, b: int, c: int) -> bytes:
-        self.log({"A": a, "B": b, "C": c})
+    def bin_shift(self, a: int, b: int, c: int, d: int):
+        self.log({"A": a, "B": b, "C": c, "D": d})
 
+        a, b, c, d = self.paginate(a, 3), self.paginate(b, 4), self.paginate(c, 5), self.paginate(d, 4)
+        text = a + b + c + d + "0" * 24
+        return int(text, 2).to_bytes(5, "big")
 
-        a, b, c = self.paginate(a, 3), self.paginate(b, 4), self.paginate(c, 4)
-        text = a + b + c + "0" * 29
+    def bin_bitreverse(self, a: int, b: int, c: int, d: int) -> bytes:
+        self.log({"A": a, "B": b, "C": c, "D": d})
+
+        a, b, c, d = self.paginate(a, 3), self.paginate(b, 4), self.paginate(c, 5), self.paginate(d, 4)
+        text = a + b + c + d + "0" * 24
         return int(text, 2).to_bytes(5, "big")
 
     def log(self, text: dict, method="last"):
@@ -69,34 +76,19 @@ class Assembler:
             lines = f.readlines()
         for line in lines:
             # Загрузка константы
-            if re.match(r"[a-zA-Z][a-zA-Z0-9]*=[0-9]+", line):
-                var, value = line.split('=')
+            if line.startswith("set"):
+                _, var, value = line.split()
                 var, value = var.strip(), int(value.strip())
+
                 address = self.add_var_to_namespace(var)
                 binary = self.bin_constant(7, address, value)
                 self.write_to_binary(binary)
 
-            elif re.match(r"[a-zA-Z][a-zA-Z0-9]*=bitreverse\([a-zA-Z][a-zA-Z0-9]*\)", line):
-                l = line.split("=")
-                var1 = l[0].strip()
-                var2 = l[1].replace("(", " ").replace(")", " ").split()[1].strip()
-
-                if var2 not in self.NAMESPACE.keys():
-                    raise Exception(f"Переменная '{var2}' не была объявлена.")
-
-                if var1 not in self.NAMESPACE.keys():
-                    address1 = self.add_var_to_namespace(var1)
-                else:
-                    address1 = self.NAMESPACE[var1]
-
-                address2 = self.NAMESPACE[var2]
-                binary = self.bin_bitreverse(4, address1, address2)
-                self.write_to_binary(binary)
-
-            # Чтение и запись в память
-            elif re.match(r"[a-zA-Z][a-zA-Z0-9]*=[a-zA-Z][a-zA-Z0-9]*", line):
-                var1, var2 = line.split('=')
+            # Чтение и запись в памяти
+            elif line.startswith("mov"):
+                _, var1, var2 = line.split()
                 var1, var2 = var1.strip(), var2.strip()
+
                 if var2 not in self.NAMESPACE.keys():
                     raise Exception(f"Переменная '{var2}' не была объявлена.")
 
@@ -109,12 +101,42 @@ class Assembler:
                 binary = self.bin_read(3, address1, address2)
                 self.write_to_binary(binary)
 
+            # Чтение из памяти и запись по адресу со сдвигом
+            elif line.startswith("shift"):
+                _, var1, shift, var2 = line.split()
+                var1, shift, var2 = var1.strip(), int(shift.strip()), var2.strip()
+
+                if var2 not in self.NAMESPACE.keys():
+                    raise Exception(f"Переменная '{var2}' не была объявлена.")
+                if var1 not in self.NAMESPACE.keys():
+                    address1 = self.add_var_to_namespace(var1)
+                else:
+                    address1 = self.NAMESPACE[var1]
+
+                address2 = self.NAMESPACE[var2]
+                binary = self.bin_shift(1, address1, shift, address2)
+                self.write_to_binary(binary)
+
+            # Унарная операция bitreverse
+            elif line.startswith("bitreverse"):
+                _, var1, shift, var2 = line.split()
+                var1, shift, var2 = var1.strip(), int(shift.strip()), var2.strip()
+
+                if var2 not in self.NAMESPACE.keys():
+                    raise Exception(f"Переменная '{var2}' не была объявлена.")
+                if var1 not in self.NAMESPACE.keys():
+                    address1 = self.add_var_to_namespace(var1)
+                else:
+                    address1 = self.NAMESPACE[var1]
+
+                address2 = self.NAMESPACE[var2]
+                binary = self.bin_bitreverse(4, address1, shift, address2)
+                self.write_to_binary(binary)
         self.dump_log()
 
 
 class Interpreter:
     def __init__(self, path_to_binary: str, path_to_result):
-        from pathlib import Path
         with open(path_to_binary, 'rb') as f:
             self.BINARY = f.read()
         self.MEMORY = [0 for _ in range(16)]
@@ -124,6 +146,7 @@ class Interpreter:
     def run(self):
         bits = bin(int.from_bytes(self.BINARY))[2:]
         commands = [bits[i:i+5*8] for i in range(0, len(bits), 8*5)]
+
         for command in commands:
             command_type = command[0:3]
             if command_type == "111":
@@ -134,12 +157,21 @@ class Interpreter:
                 address1 = int(command[3:7], 2)
                 address2 = int(command[7:11], 2)
                 self.MEMORY[address1] = self.MEMORY[address2]
+
+            elif command_type == "001":
+                address1 = int(command[3:7], 2)
+                shift = int(command[7:12], 2)
+                address2 = int(command[12:16], 2)
+                self.MEMORY[address1 + shift] = self.MEMORY[address2]
+
             elif command_type == "100":
                 address1 = int(command[3:7], 2)
-                address2 = int(command[7:11], 2)
+                shift = int(command[7:12], 2)
+                address2 = int(command[12:16], 2)
+
                 value = self.MEMORY[address2]
                 reversed = int(bin(value)[2:].zfill(28)[::-1], 2)
-                self.MEMORY[address1] = reversed
+                self.MEMORY[address1 + shift] = reversed
 
         self.log_result()
 
